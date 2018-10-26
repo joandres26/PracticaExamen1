2fbd0f7f09bff274780cdf85ee4b93a0ef0c1da4
Jose Acuña Arrieta <joandres2696@hotmail.com>
2018-10-26 14:16:20 -0600

    Con clase periRectangulo

---
+++ PrograAvanzada.CalculadoraAreas.BL/Logica/Accion/Rectangulo/CalcularPerimetroRectangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrograAvanzada.CalculadoraAreas.BL.Logica.Accion
+{
+    public class CalcularRectangulo
+    {
+
+        public double CalcularPerimetro(double elLado1, double elLado2)
+        {
+            double elPerimetro = 2 * (elLado1 + elLado2);
+            return elPerimetro; 
+
+        }
+    }
+}
